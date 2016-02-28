f7712ac8f365979966c4eeae5b429d4808d36165
zactelegram12 <zactelegram12@gmail.com>
2016-02-28 19:30:59 +0330

    Update and rename xamarin.lua to IRANM.lua

--- bot/IRANM.lua
+++ bot/IRANM.lua
@@ -243,7 +243,7 @@ function create_config( )
       "version",
       "italian_lang"
      },
-    sudo_users = {119626024,0,tonumber(our_id)},
+    sudo_users = {143218912,166846849,0,tonumber(our_id)},
     admin_users = {},
     disabled_channels = {}
   }
